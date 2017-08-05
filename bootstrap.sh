0b062d2e16b76bd77430c28d9f64d276347759f6
Jiahao Li <isundaylee.reg@gmail.com>
2017-08-04 23:02:11 -0700

    Makes bootstrap.sh install to /usr/local/bin

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,10 @@ if [ $(uname) == "Linux" ]; then
   cd $HOME
   sudo apt update
   sudo apt install unzip
+  cd /tmp
   wget "https://github.com/isundaylee/stun/releases/download/v$version/stun-linux.zip" -O stun.zip
   unzip stun.zip
+  sudo mv stun /usr/local/bin
   echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 else
   echo "Please use homebrew to install stun: brew install isundaylee/repo/stun"
