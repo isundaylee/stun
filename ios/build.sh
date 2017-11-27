3202d7f73986fdb19b08b4dcad577d6fc073e1a5
Jiahao Li <isundaylee.reg@gmail.com>
2017-11-26 19:23:43 -0500

    Fixes plists for TestFlight submission

--- ios/build.sh
+++ ios/build.sh
@@ -3,6 +3,8 @@
 SOURCE_APP="buck-out/gen/ios/Stun#dwarf-and-dsym,iphoneos-arm64,no-include-frameworks/Stun.app"
 TMP_APP="/tmp/Stun.app"
 TMP_EXT="/tmp/Stun.app/PlugIns/StunPacketTunnel.appex"
+TMP_IPA="/tmp/Stun.ipa"
+TMP_PAYLOAD="/tmp/Stun.ipa/Payload"
 
 PROVISION_FILE="/tmp/stun_provision.plist"
 ENTITLEMENTS_FILE="/tmp/stun_entitlements.plist"
@@ -14,15 +16,25 @@ buck build "//ios:Stun#iphoneos-arm64"
 rm -rf "$TMP_APP"
 cp -r "$SOURCE_APP" "$TMP_APP"
 
-# Sign the app
-security cms -D -i certs/Stun_Dev.mobileprovision > "$PROVISION_FILE"
-/usr/libexec/PlistBuddy -x -c 'Print :Entitlements' "$PROVISION_FILE" > "$ENTITLEMENTS_FILE"
-/usr/bin/codesign -f -s 'iPhone Developer: Jiahao Li' --entitlements "$ENTITLEMENTS_FILE" "$TMP_APP"
-
 # Sign the extension
-security cms -D -i certs/Stun_Packet_Tunnel_Dev.mobileprovision > "$PROVISION_FILE"
+rm -r $TMP_EXT/_CodeSignature
+cp certs/Stun_Packet_Tunnel_Distr.mobileprovision "$TMP_EXT/embedded.mobileprovision"
+security cms -D -i certs/Stun_Packet_Tunnel_Distr.mobileprovision > "$PROVISION_FILE"
 /usr/libexec/PlistBuddy -x -c 'Print :Entitlements' "$PROVISION_FILE" > "$ENTITLEMENTS_FILE"
-/usr/bin/codesign -f -s 'iPhone Developer: Jiahao Li' --entitlements "$ENTITLEMENTS_FILE" "$TMP_EXT"
+/usr/bin/codesign -f -s 'iPhone Distribution: Jiahao Li' -vv --entitlements "$ENTITLEMENTS_FILE" "$TMP_EXT"
+
+# Sign the app
+rm -r $TMP_APP/_CodeSignature
+cp certs/Stun_Distr.mobileprovision "$TMP_APP/embedded.mobileprovision"
+security cms -D -i certs/Stun_Distr.mobileprovision > "$PROVISION_FILE"
+/usr/libexec/PlistBuddy -x -c 'Print :Entitlements' "$PROVISION_FILE" > "$ENTITLEMENTS_FILE"
+/usr/bin/codesign -f -s 'iPhone Distribution: Jiahao Li' -vv --entitlements "$ENTITLEMENTS_FILE" "$TMP_APP"
+
+rm -r $TMP_IPA
+mkdir -p $TMP_PAYLOAD
+cp -r $TMP_APP $TMP_PAYLOAD
+cd $TMP_IPA
+zip -r $TMP_IPA/Stun.resigned.ipa Payload
 
 # Install
 echo "Please use XCode to deploy $TMP_APP to the device."
