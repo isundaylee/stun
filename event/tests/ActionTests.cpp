f318612128ffae19fc46463f40d4870fe9f73ae8
Jiahao Li <reg@ljh.me>
2019-09-12 22:34:32 -0700

    [Core] Implements basic Action tests

--- event/tests/ActionTests.cpp
+++ event/tests/ActionTests.cpp
@@ -1,3 +1,63 @@
 #include "gtest/gtest.h"
 
-TEST(ActionTests, TrivialTest) { ASSERT_EQ(0, 0) << "0 should be equal to 0"; }
+#include <event/Action.h>
+
+class TestTrigger {
+public:
+  void fire() { fired_ = true; }
+
+  bool hasFired() const { return fired_; }
+
+private:
+  bool fired_ = false;
+};
+
+TEST(ActionTests, AlwaysOnAction) {
+  event::EventLoop loop;
+
+  auto trigger = TestTrigger{};
+  auto action = loop.createAction({});
+  action->callback = [&trigger]() { trigger.fire(); };
+
+  loop.runOnce();
+  ASSERT_TRUE(trigger.hasFired()) << "Trigger should have fired.";
+}
+
+TEST(ActionTests, SingleBaseConditionAction) {
+  event::EventLoop loop;
+
+  auto trigger = TestTrigger{};
+  auto condition = loop.createBaseCondition();
+  auto action = event::Action{loop, {condition.get()}};
+  action.callback = [&trigger]() { trigger.fire(); };
+
+  loop.runOnce();
+  ASSERT_FALSE(trigger.hasFired()) << "Trigger should not have fired.";
+
+  condition->fire();
+  loop.runOnce();
+  ASSERT_TRUE(trigger.hasFired()) << "Trigger should have fired.";
+}
+
+TEST(ActionTests, MultipleBaseConditionsAction) {
+  event::EventLoop loop;
+
+  auto trigger = TestTrigger{};
+  auto condition1 = loop.createBaseCondition();
+  auto condition2 = loop.createBaseCondition();
+  auto action = event::Action{loop, {condition1.get(), condition2.get()}};
+  action.callback = [&trigger]() { trigger.fire(); };
+
+  loop.runOnce();
+  ASSERT_FALSE(trigger.hasFired()) << "Trigger should not have fired.";
+
+  condition1->fire();
+  loop.runOnce();
+  ASSERT_FALSE(trigger.hasFired())
+      << "Trigger should not have fired after condition 1 fires.";
+
+  condition2->fire();
+  loop.runOnce();
+  ASSERT_TRUE(trigger.hasFired())
+      << "Trigger should have fired after both conditions fire.";
+}
