f92a9d01fe3561c414cc54536833ea32fa81a2e9
Jiahao Li <reg@ljh.me>
2019-12-27 22:55:31 -0500

    [Test] Randomize test subnet

--- test/basic.py
+++ test/basic.py
@@ -7,6 +7,7 @@ import json
 import shutil
 import time
 import uuid
+import random
 
 tempfile.tempdir = "/tmp"
 
@@ -28,18 +29,18 @@ def docker(args, assert_on_failure=True, input=None):
     return (result.stdout.decode(), result.stderr.decode(), result.returncode)
 
 
-def get_server_config(**kwargs):
+def get_server_config(test_context, **kwargs):
     return {
         "role": "server",
         "secret": "lovely",
-        "address_pool": "10.179.0.0/24",
+        "address_pool": f"{test_context.get_ip(0)}/24",
         "padding_to": 1000,
         "data_pipe_rotate_interval": 60,
         **kwargs,
     }
 
 
-def get_client_config(**kwargs):
+def get_client_config(test_context, **kwargs):
     return {
         "role": "client",
         "server": "server",
@@ -58,6 +59,10 @@ DOCKER_IMAGE_TAG = "stun_test"
 class Context(object):
     def __init__(self):
         self.uuid = uuid.uuid1().hex
+        self.subnet_id = random.randint(1, 254)
+
+    def get_ip(self, last_octet):
+        return f"10.179.{self.subnet_id}.{last_octet}"
 
 
 class Host:
@@ -209,43 +214,55 @@ class TestBasic(unittest.TestCase):
         docker(["network", "rm", DOCKER_NETWORK_NAME + "-" + self.test_context.uuid])
 
     def test_basic(self):
-        with Host(self.test_context, "server", get_server_config()) as server, Host(
-            self.test_context, "client", get_client_config()
+        with Host(
+            self.test_context, "server", get_server_config(self.test_context)
+        ) as server, Host(
+            self.test_context, "client", get_client_config(self.test_context)
         ) as client:
 
             self.assertEqual(
-                client.exec(["ping", "-t", "1", "-c", "1", "10.179.0.1"])[2],
+                client.exec(
+                    ["ping", "-t", "1", "-c", "1", f"{self.test_context.get_ip(1)}"]
+                )[2],
                 0,
                 "Failed to ping from client to server.",
             )
 
     def test_static_hosts(self):
         server_config = get_server_config(
-            authentication=True, static_hosts={"nice_guy": "10.179.0.152"}
+            self.test_context,
+            authentication=True,
+            static_hosts={"nice_guy": f"{self.test_context.get_ip(152)}"},
         )
 
-        client_config = get_client_config(user="nice_guy")
+        client_config = get_client_config(self.test_context, user="nice_guy")
 
         with Host(self.test_context, "server", server_config) as server, Host(
             self.test_context, "client", client_config
         ) as client:
 
             self.assertEqual(
-                client.exec(["ping", "-t", "1", "-c", "1", "10.179.0.1"])[2],
+                client.exec(
+                    ["ping", "-t", "1", "-c", "1", f"{self.test_context.get_ip(1)}"]
+                )[2],
                 0,
                 "Failed to ping from client to server.",
             )
 
             self.assertEqual(
                 client.get_client_tunnel_ip(),
-                "10.179.0.152",
+                f"{self.test_context.get_ip(152)}",
                 "Unexpected IP assigned to client.",
             )
 
     def test_authentication_without_username(self):
         with Host(
-            self.test_context, "server", get_server_config(authentication=True)
-        ) as server, Host(self.test_context, "client", get_client_config()) as client:
+            self.test_context,
+            "server",
+            get_server_config(self.test_context, authentication=True),
+        ) as server, Host(
+            self.test_context, "client", get_client_config(self.test_context)
+        ) as client:
 
             self.assertIn(
                 "Session ended with error: No user name provided.",
@@ -254,20 +271,26 @@ class TestBasic(unittest.TestCase):
             )
 
             self.assertEqual(
-                client.exec(["ping", "-t", "1", "-c", "1", "10.179.0.1"])[2],
+                client.exec(
+                    ["ping", "-t", "1", "-c", "1", f"{self.test_context.get_ip(1)}"]
+                )[2],
                 1,
                 "Should not be able to ping from client to server.",
             )
 
     def test_provided_subnets(self):
-        with Host(self.test_context, "server", get_server_config()) as server, Host(
+        with Host(
+            self.test_context, "server", get_server_config(self.test_context)
+        ) as server, Host(
             self.test_context,
             "client",
-            get_client_config(provided_subnets=["10.180.0.0/24"]),
+            get_client_config(self.test_context, provided_subnets=["10.180.0.0/24"]),
         ) as client:
 
             self.assertEqual(
-                client.exec(["ping", "-t", "1", "-c", "1", "10.179.0.1"])[2],
+                client.exec(
+                    ["ping", "-t", "1", "-c", "1", f"{self.test_context.get_ip(1)}"]
+                )[2],
                 0,
                 "Failed to ping from client to server.",
             )
@@ -276,28 +299,35 @@ class TestBasic(unittest.TestCase):
                 "\n"
             )
             self.assertIn(
-                "10.180.0.0/24 via 10.179.0.2 dev tun0 ",
+                f"10.180.0.0/24 via {self.test_context.get_ip(2)} dev tun0 ",
                 route_lines,
                 "Server should have added a route for the client-provided subnet.",
             )
 
     def test_custom_port(self):
         with Host(
-            self.test_context, "server", get_server_config(port=1099)
+            self.test_context, "server", get_server_config(self.test_context, port=1099)
         ) as server, Host(
-            self.test_context, "client", get_client_config(port=1099)
+            self.test_context, "client", get_client_config(self.test_context, port=1099)
         ) as client:
 
             self.assertEqual(
-                client.exec(["ping", "-t", "1", "-c", "1", "10.179.0.1"])[2],
+                client.exec(
+                    ["ping", "-t", "1", "-c", "1", f"{self.test_context.get_ip(1)}"]
+                )[2],
                 0,
                 "Failed to ping from client to server.",
             )
 
     def test_multiple_servers_iptables_rule(self):
-        with Host(self.test_context, "server", get_server_config()) as server:
+        with Host(
+            self.test_context, "server", get_server_config(self.test_context)
+        ) as server:
             server.run_stun(
-                "extra", get_server_config(address_pool="10.180.0.0/24", port=1099)
+                "extra",
+                get_server_config(
+                    self.test_context, address_pool="10.180.0.0/24", port=1099
+                ),
             )
 
             masqueraded_subnets = server.get_masqueraded_subnets()
@@ -307,9 +337,9 @@ class TestBasic(unittest.TestCase):
             )
 
             self.assertIn(
-                "10.179.0.0/24",
+                f"{self.test_context.get_ip(0)}/24",
                 masqueraded_subnets,
-                "10.179.0.0/24 should be MASQUERADE-d.",
+                f"{self.test_context.get_ip(0)}/24 should be MASQUERADE-d.",
             )
 
             self.assertIn(
@@ -319,15 +349,23 @@ class TestBasic(unittest.TestCase):
             )
 
     def test_multiple_servers_iptables_rule_cleared_properly(self):
-        with Host(self.test_context, "server", get_server_config()) as server:
+        with Host(
+            self.test_context, "server", get_server_config(self.test_context)
+        ) as server:
             server.run_stun(
-                "extra", get_server_config(address_pool="10.180.0.0/24", port=1099)
+                "extra",
+                get_server_config(
+                    self.test_context, address_pool="10.180.0.0/24", port=1099
+                ),
             )
 
             server.kill_stun("extra")
 
             server.run_stun(
-                "extra", get_server_config(address_pool="10.180.0.0/24", port=1099)
+                "extra",
+                get_server_config(
+                    self.test_context, address_pool="10.180.0.0/24", port=1099
+                ),
             )
 
             masqueraded_subnets = server.get_masqueraded_subnets()
@@ -337,9 +375,9 @@ class TestBasic(unittest.TestCase):
             )
 
             self.assertIn(
-                "10.179.0.0/24",
+                f"{self.test_context.get_ip(0)}/24",
                 masqueraded_subnets,
-                "10.179.0.0/24 should be MASQUERADE-d.",
+                f"{self.test_context.get_ip(0)}/24 should be MASQUERADE-d.",
             )
 
             self.assertIn(
@@ -354,12 +392,12 @@ class TestBasic(unittest.TestCase):
         with Host(
             self.test_context,
             "server",
-            get_server_config(**server_config),
+            get_server_config(self.test_context, **server_config),
             entry_args=["-v"],
         ) as server, Host(
             self.test_context,
             "client",
-            get_client_config(user="test-client"),
+            get_client_config(self.test_context, user="test-client"),
             entry_args=["-v"],
         ) as client:
 
@@ -376,9 +414,15 @@ class TestBasic(unittest.TestCase):
 
     def test_per_user_log_message_without_authentication(self):
         with Host(
-            self.test_context, "server", get_server_config(), entry_args=["-v"]
+            self.test_context,
+            "server",
+            get_server_config(self.test_context),
+            entry_args=["-v"],
         ) as server, Host(
-            self.test_context, "client", get_client_config(), entry_args=["-v"]
+            self.test_context,
+            "client",
+            get_client_config(self.test_context),
+            entry_args=["-v"],
         ) as client:
 
             server_expected_messages = [
@@ -396,7 +440,7 @@ class TestBasic(unittest.TestCase):
         with Host(
             self.test_context,
             "server",
-            get_server_config(masquerade_output_interface="eth0"),
+            get_server_config(self.test_context, masquerade_output_interface="eth0"),
         ) as server:
             entries = server.exec(
                 ["iptables", "-t", "nat", "-S"], assert_on_failure=True
@@ -404,31 +448,42 @@ class TestBasic(unittest.TestCase):
 
             found = False
             for entry in entries:
-                if "-A POSTROUTING -s 10.179.0.0/24 -o eth0 -m comment" in entry:
+                if (
+                    f"-A POSTROUTING -s {self.test_context.get_ip(0)}/24 -o eth0 -m comment"
+                    in entry
+                ):
                     found = True
 
             self.assertTrue(found, "Expected iptables rule not found.")
 
     def test_masquerade_output_interface_empty(self):
-        with Host(self.test_context, "server", get_server_config()) as server:
+        with Host(
+            self.test_context, "server", get_server_config(self.test_context)
+        ) as server:
             entries = server.exec(
                 ["iptables", "-t", "nat", "-S"], assert_on_failure=True
             )[0].split("\n")
 
             found = False
             for entry in entries:
-                if "-A POSTROUTING -s 10.179.0.0/24 -m comment" in entry:
+                if (
+                    f"-A POSTROUTING -s {self.test_context.get_ip(0)}/24 -m comment"
+                    in entry
+                ):
                     found = True
 
             self.assertTrue(found, "Expected iptables rule not found.")
 
     def test_malformatted_provided_subnet(self):
         with Host(
-            self.test_context, "server", get_server_config(), entry_args=["-v"]
+            self.test_context,
+            "server",
+            get_server_config(self.test_context),
+            entry_args=["-v"],
         ) as server, Host(
             self.test_context,
             "client",
-            get_client_config(provided_subnets=["10.179.10.1/24"]),
+            get_client_config(self.test_context, provided_subnets=["10.179.10.1/24"]),
         ) as client:
 
             self.assertIn(
@@ -437,13 +492,28 @@ class TestBasic(unittest.TestCase):
 
     def test_loss_estimator(self):
         with Host(
-            self.test_context, "server", get_server_config(), entry_args=["-v"]
+            self.test_context,
+            "server",
+            get_server_config(self.test_context),
+            entry_args=["-v"],
         ) as server, Host(
-            self.test_context, "client", get_client_config(), entry_args=["-v"]
+            self.test_context,
+            "client",
+            get_client_config(self.test_context),
+            entry_args=["-v"],
         ) as client:
 
             client.exec(
-                ["ping", "-i", "0.1", "-t", "1", "-c", "10", "10.179.0.1"],
+                [
+                    "ping",
+                    "-i",
+                    "0.1",
+                    "-t",
+                    "1",
+                    "-c",
+                    "10",
+                    f"{self.test_context.get_ip(1)}",
+                ],
                 assert_on_failure=True,
             )
             time.sleep(1)
@@ -461,9 +531,15 @@ class TestBasic(unittest.TestCase):
 
     def test_data_pipe_preference_default(self):
         with Host(
-            self.test_context, "server", get_server_config(), entry_args=["-v"]
+            self.test_context,
+            "server",
+            get_server_config(self.test_context),
+            entry_args=["-v"],
         ) as server, Host(
-            self.test_context, "client", get_client_config(), entry_args=["-v"]
+            self.test_context,
+            "client",
+            get_client_config(self.test_context),
+            entry_args=["-v"],
         ) as client:
             time.sleep(1)
 
@@ -481,11 +557,14 @@ class TestBasic(unittest.TestCase):
 
     def test_data_pipe_preference_specified(self):
         with Host(
-            self.test_context, "server", get_server_config(), entry_args=["-v"]
+            self.test_context,
+            "server",
+            get_server_config(self.test_context),
+            entry_args=["-v"],
         ) as server, Host(
             self.test_context,
             "client",
-            get_client_config(data_pipe_preference=["tcp", "udp"]),
+            get_client_config(self.test_context, data_pipe_preference=["tcp", "udp"]),
             entry_args=["-v"],
         ) as client:
             time.sleep(1)
